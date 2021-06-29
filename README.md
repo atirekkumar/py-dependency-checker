d0b247684cadc6768b6bf3aa9d4851150ae9ff27
Atirek <atirek.kumar@couture.ai>
2021-06-29 06:44:19 +0530

    Switched compulsorily to python3. Added conda comments

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
-# Requirements  
-Install pipdeptree 
+# How to run
+1. First run the script as ```./script.sh``` to check from broken dependencies.
+2. If there are any broken dependencies, fix them first before checking for installation of new package.
+3. To check if a new package will cause dependency issues, run the script as: 
 ```
-pip install pipdeptree
+./script.sh <package-name>
 ```
+
+# Possible bugs
+Make sure you are running python3 (and associated pip) and not python2.
 
 # Resources  
 - https://stackoverflow.com/questions/3503879/assign-output-of-os-system-to-a-variable-and-prevent-it-from-being-displayed-on  
--- script.sh
+++ script.sh
@@ -1,22 +1,37 @@
 #!/bin/bash
 
+# conda-pip incompatibility: https://stackoverflow.com/questions/50777849/from-conda-create-requirements-txt-for-pip3 , https://www.anaconda.com/blog/using-pip-in-a-conda-environment
+# relevant to error message: http://5.9.10.113/63480238/deployment-in-heroku-fails-due-to-issue-in-requirements-txt
+# Error message: ERROR: Could not install packages due to an EnvironmentError: [Errno 2] No such file or directory: '/home/conda/feedstock_root/build_artifacts/backcall_1592338393461/work'
+# pip-freeze path problem: https://stackoverflow.com/questions/62885911/pip-freeze-creates-some-weird-path-instead-of-the-package-version/62886215#62886215
+
+# check python version
+
 # check existing dependencies
-pip check
+pip3 check
 
 # to check a new package
 if [[ $# -gt 0 ]]
 then
-    name="dependency"
-#     pip freeze > requirements.txt
-    conda install -e > requirements.txt
+    name="dependency145"
+    # conda list -e > requirements.txt
+    pip3 freeze > requirements.txt
+    # pip3 list --format=freeze > requirements.txt
+    # conda create --name $name
+    # conda create --file requirements.txt
     # creating and entering venv
     python3 -m venv $name
     source $name/bin/activate
     # installing requirements
-#     pip install -r requirements.txt
-    conda install --file requirements.txt
-    conda install $1
-    pip check
+    echo "Installing pre-existing packages in virtualenv..."
+    pip3 -q install -r requirements.txt
+    echo "Done."
+    # conda install --file requirements.txt
+    echo "Installing $1"
+    pip3 -q install $1
+    echo "Done."
+    # conda install $1
+    pip3 check
     deactivate
     # deleting the venv
     rm -rf $name
