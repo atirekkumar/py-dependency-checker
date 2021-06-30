b6a8d8dab0a94bd63e2f5d64ea30057e075dcb74
Atirek <atirek.kumar@couture.ai>
2021-06-30 12:09:14 +0530

    fixed bugs: pip version

--- script.sh
+++ script.sh
@@ -4,7 +4,7 @@
 # relevant to error message: http://5.9.10.113/63480238/deployment-in-heroku-fails-due-to-issue-in-requirements-txt
 # Error message: ERROR: Could not install packages due to an EnvironmentError: [Errno 2] No such file or directory: '/home/conda/feedstock_root/build_artifacts/backcall_1592338393461/work'
 # pip-freeze path problem: https://stackoverflow.com/questions/62885911/pip-freeze-creates-some-weird-path-instead-of-the-package-version/62886215#62886215
-
+# ERROR: "Could not find a version that satisfies the requirement". This error can be caused by the package version being required not matching with the installed python or pip version
 # check python version
 
 # check existing dependencies
@@ -14,14 +14,21 @@ pip3 check
 if [[ $# -gt 0 ]]
 then
     name="dependency145"
-    # conda list -e > requirements.txt
+    pip_version=20.1
     pip3 freeze > requirements.txt
-    # pip3 list --format=freeze > requirements.txt
-    # conda create --name $name
-    # conda create --file requirements.txt
+    pip3 install virtualenv
     # creating and entering venv
+    # venv cannot edit the python version being used. Hence to use a different python version, use virtualenv
     python3 -m venv $name
+    # conda create -n $name python=3.5
     source $name/bin/activate
+    # conda activate $name
+
+    # get correct pip version
+    # https://stackoverflow.com/questions/21099057/control-the-pip-version-in-virtualenv
+    curl https://bootstrap.pypa.io/pip/3.5/get-pip.py -o get-pip.py
+    python3 get-pip.py pip==$pip_version  # also check if pip 8.1.1 is downloaded and can be deleted
+
     # installing requirements
     echo "Installing pre-existing packages in virtualenv..."
     pip3 -q install -r requirements.txt
